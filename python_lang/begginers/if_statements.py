a07e6cc5d46e3b8e7106407d20d748fb430f8eff
Ashkan Mohammadi <mohammadiashkan1384@gmail.com>
2021-07-25 21:23:51 +0430

    tutorial for if statements

---
+++ python_lang/begginers/if_statements.py
@@ -0,0 +1,12 @@
+my_age = 16
+
+your_age = int(input("please enter your age: "))
+
+if your_age > my_age:
+    print ("you are older than me")
+elif your_age == my_age:
+    print ("we are at the same age")
+else:
+    print ("you are younger than me")
+
+# "10" -> 10
