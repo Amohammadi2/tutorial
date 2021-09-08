e6a41ae1dcd5fa1527fe86be30006a34078d8382
Ashkan Mohammadi <mohammadiashkan1384@gmail.com>
2021-09-08 22:21:07 +0430

    [feat] add `turtle` lib examples

---
+++ python_lang/begginers/turtles/complex_shape.py
@@ -0,0 +1,7 @@
+import turtle
+
+for i in range(8):
+    turtle.forward(100)
+    turtle.left(45)
+
+input()
---
+++ python_lang/begginers/turtles/hexagon.py
@@ -0,0 +1,7 @@
+import turtle
+
+for i in range(2, 200, 2):
+    turtle.forward(i)
+    turtle.left(90)
+
+input()
