1479cc8fc7e2a7793ddfd1741b8cd3a19ec59e73
Ashkan Mohammadi <mohammadiashkan1384@gmail.com>
2021-09-22 22:30:15 +0330

    [feat] add example for `break` and `continue`

--- python_lang/begginers/for_statements.py
+++ python_lang/begginers/for_statements.py
@@ -5,5 +5,17 @@
 
 # 0-9
 # 0 1 2 3 4 ... 9
-for i in range(10, 2, -1):
-    print(i)
+# for i in range(10, 2, -1):
+#     print(i)
+
+
+# {1, ..., 10}
+for n in range(1, 100):
+    if n == 1:
+        continue
+    for x in range(2, n):
+        if n % x == 0:
+            print(f'{n} is not prime')
+            break
+    else:
+        print(f'{n} is prime')
